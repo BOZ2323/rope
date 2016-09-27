50eaa6d0767224e476e845f4b326355829cb50df
BOZ2323 <lillibaaske@gmx.de>
2016-09-27 13:56:01 +0200

    Add more points and put y coordinates all in one array

--- rope.js
+++ rope.js
@@ -1,13 +1,42 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var point_y = [ 300, 400, 500, 600 ];
+
+
+function point(x, y)
+{
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2 * Math.PI, true);
+    ctx.fill();
+}
+
+point(700,point_y[1]);
 
 
 
 function point(x, y)
 {
     ctx.beginPath();
-    ctx.arc(x, y, 1, 0, 2 * Math.PI, true);
+    ctx.arc(x, y, 10, 0, 2 * Math.PI, true);
     ctx.fill();
 }
 
-point(20,40);
+point(600,point_y[2]);
+
+function point(x, y)
+{
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2 * Math.PI, true);
+    ctx.fill();
+}
+
+point(500,point_y[3]);
+
+function point(x,y)
+{
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2 * Math.PI, true);
+    ctx.fill();
+}
+
+point(300,point_y[4]);
