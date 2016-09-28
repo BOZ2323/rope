c8271eee3758c3230d9a72a1025e40dbdac6ff95
BOZ2323 <lillibaaske@gmx.de>
2016-09-28 21:54:52 +0200

    Draw point and lines in canvas to start the rope

--- rope.js
+++ rope.js
@@ -1,7 +1,28 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var point_y = [ 300, 400, 500, 600 ];
+var point_y = [ 200, 300, 500 ];
 
+
+function point(x, y)
+{
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2 * Math.PI, true);
+    ctx.fillStyle = "red";
+    ctx.fill();
+}
+
+point(200,point_y[0]);
+
+function line()
+{   
+    ctx.beginPath();    
+    ctx.moveTo (200,200);
+    ctx.lineTo (700,300);
+    ctx.lineTo (500,500);
+    ctx.stroke();
+}
+
+line();
 
 function point(x, y)
 {
@@ -12,8 +33,6 @@ function point(x, y)
 
 point(700,point_y[1]);
 
-
-
 function point(x, y)
 {
     ctx.beginPath();
@@ -21,22 +40,5 @@ function point(x, y)
     ctx.fill();
 }
 
-point(600,point_y[2]);
+point(500,point_y[2]);
 
-function point(x, y)
-{
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, 2 * Math.PI, true);
-    ctx.fill();
-}
-
-point(500,point_y[3]);
-
-function point(x,y)
-{
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, 2 * Math.PI, true);
-    ctx.fill();
-}
-
-point(300,point_y[4]);
