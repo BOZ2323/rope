f2b41be409a1da787d0322a455ab7003918d0bc4
BOZ2323 <lillibaaske@gmx.de>
2016-10-11 13:36:52 +0200

    Adjusted variable names and tried the push method

--- rope.js
+++ rope.js
@@ -1,44 +1,51 @@
-function drawRope(horizontalPositions, verticalPositions, radius)
+function drawRope(x_positions, y_positions, radius)
 {
-    if (horizontalPositions.length !== verticalPositions.length) {
-        throw new Error("horizontalPositions.length does not match verticalPositions.length");
-    }
+    //if (xps.length !== yps.length) {
+      //  throw new Error("horizontalPositions.length does not match verticalPositions.length");
+   // }
 
-    for(var i=0; i<horizontalPositions.length; i++) {
-        var x = horizontalPositions[i];
-        var y = verticalPositions[i];   
+    for(var i=0; i<x_positions.length; i++) {
+        var x = x_positions[i];
+        var y = y_positions[i];   
 
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, 2 * Math.PI);
         ctx.fill();
 
-        if (i+1 < horizontalPositions.length) {
+        if (i+1 < x_positions.length) {
             ctx.moveTo(x, y); 
-            ctx.lineTo(horizontalPositions[i+1], verticalPositions[i+1]);
+            ctx.lineTo(x_positions[i+1], y_positions[i+1]);
             ctx.stroke();
         }
     }
 }
 
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var x_positions = [20, 40, 200, 250, 260];
 
-function sinusShape(xps) {
-    for (var i=0; i < xps.length; i++){
-        var y_positions  = Math.sin(xps[i] * Math.PI / 180) * -120 + 180;
-        console.log(y_positions + "Hallo");
 
-            // -120 = Skalierungsfaktor (Amplitüde)
-            // 180 = Translation (Verschiebung auf der y-Achse)
-            // PI/180 = SKalierung von x (ist kleiner als 1, daher: verringert die Frequenz)
+function sinusShape(x_positions) {
+    var y_positions = [];
+    for (var i=0; i < x_positions.length; i++){
+        var y = Math.sin(x_positions[i] * Math.PI / 180) * -120 + 180;
+        console.log(y);
+        y_positions.push(y);
+
+        //function sinusShape(xps) {
+        //  for (var i=0; i < xps.length; i++){
+        //    var y_positions  = Math.sin(xps[i] * Math.PI / 180) * -120 + 180;
+        //  console.log(y_positions + "Hallo");
+
+        // -120 = Skalierungsfaktor (Amplitüde)
+        // 180 = Translation (Verschiebung auf der y-Achse)
+        // PI/180 = SKalierung von x (ist kleiner als 1, daher: verringert die Frequenz)
     }
 } 
 
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var x_positions = [20, 40, 200, 250, 260];
 
-// im array von x_positions werden die x positionen benannt
-// anhand dieser werte kann die funktion sinusShape die y_positions berechnen
-y_positions = sinusShape(x_positions);
-drawRope(x_positions, y_positions,6);
-
-console.log('Hallo Welt');
+    // im array von x_positions werden die x positionen benannt
+    // anhand dieser werte kann die funktion sinusShape die y_positions berechnen
+    y_positions = sinusShape(x_positions);
+    drawRope(x_positions, y_positions,6);
+    console.log('Hallo Welt');
