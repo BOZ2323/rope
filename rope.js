c48e3834d9560e54b0312890792f259e8516d542
BOZ2323 <lillibaaske@gmx.de>
2016-10-13 13:07:27 +0200

    Add function calcPosition

--- rope.js
+++ rope.js
@@ -1,52 +1,62 @@
-function drawRope(x_positions, y_positions, radius)
-{
-    if (x_positions.length !== y_positions.length) {
+function drawRope(horizontalPositions, verticalPositions, radius){
+    if (horizontalPositions.length !== verticalPositions.length) {
         throw new Error("horizontalPositions.length does not match verticalPositions.length");
     }
 
-    for(var i=0; i<x_positions.length; i++) {
-        var x = x_positions[i];
-        var y = y_positions[i];   
+    var oldx, oldy;
+    for(var i=0; i<horizontalPositions.length; i++) {
+        var x = horizontalPositions[i];
+        var y = verticalPositions[i];
 
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, 2 * Math.PI);
         ctx.fill();
 
-        if (i+1 < x_positions.length) {
+        if (i !== 0) {
             ctx.moveTo(x, y); 
-            ctx.lineTo(x_positions[i+1], y_positions[i+1]);
+            ctx.lineTo(oldx, oldy); // das objekt ctx hat die eigenschaft lineTo, die eine funktion ist. die funktion lineTo wird mir zwei parametern aufgerufen: werte oldx, oldy 
             ctx.stroke();
         }
+
+        oldx = x;
+        oldy = y;
     }
 }
 
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    var x_positions = [20, 40, 200, 250, 260];
-    var y_positions = sinusShape(x_positions);
-    // im array von x_positions werden die x positionen benannt
-    // anhand dieser werte kann die funktion sinusShape die y_positions berechnen
-    
-    
-function sinusShape(x_positions) {
-    var y_positions = [];
-    for (var i=0; i < x_positions.length; i++){
-        var y = Math.sin(x_positions[i] * Math.PI / 180) * -120 + 180;
-        console.log(y);
-        y_positions.push(y);
-        return y;
-
-        //function sinusShape(xps) {
-        //  for (var i=0; i < xps.length; i++){
-        //    var y_positions  = Math.sin(xps[i] * Math.PI / 180) * -120 + 180;
-        //  console.log(y_positions + "Hallo");
+function sinusShape(xps) {
+    var yps = [];
+    for (var i=0; i < xps.length; i++){
+        var y = Math.sin(xps[i] * Math.PI / 180) * -120 + 180;
+        yps.push(y);
 
         // -120 = Skalierungsfaktor (Amplitüde)
         // 180 = Translation (Verschiebung auf der y-Achse)
         // PI/180 = SKalierung von x (ist kleiner als 1, daher: verringert die Frequenz)
     }
+    console.log(yps);
+    return yps;
 } 
 
-sinusShape(x_positions);
+function calcXpositions(start, end, count) {
+    if (count<2) {
+        throw new Error("Count must be two or higher");
+    }
+    var distance = (end-start) / count;
+    var result = [];
+    for (var x=start; x<end; x+=distance) {
+        result.push(x);
+    }
+    console.log(x);
+    return result;
+}
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+//var x_positions = [20, 40, 200, 250, 260];
+var x_positions = calcXpositions(20, 800, 300); //der Variablen x_positions wird der return-wert zugewiesen 
+var y_positions = sinusShape(x_positions);
+// im array von x_positions werden die x positionen benannt
+// anhand dieser werte kann die funktion sinusShape die y_positions berechnen
+
 drawRope(x_positions, y_positions,6);
 console.log('Hallo Welt');
