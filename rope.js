cc210ab7c33d01089abfbfde4471bbdd6fae2fd7
BOZ2323 <lillibaaske@gmx.de>
2016-10-27 13:25:20 +0200

    Fix unitTest. Refactored tests

--- rope.js
+++ rope.js
@@ -38,7 +38,6 @@ function sinusShape(xps) {
     return yps;
 } 
 
-/*
 
 function calcXpositions(start, end, count) {
     if (count<2) {
@@ -54,32 +53,9 @@ function calcXpositions(start, end, count) {
     }
     console.log (xCoordinates);
     return xCoordinates;
-    
-}
-*/
-
-function calcXpositions(start, end, count) {
-    try {
-    if (count<2) {
-        throw new Error("Count must be two or higher");
-    }
-    var distance = (end-start) / (count - 1);
-    console.log(distance);
-
-    var xCoordinates = [];  //evtl. die Strecke erst halbieren und dann nochmal in kleinere Teile zerlegen um Fehler zu verteilen
-    for (var n=0; n<count; n++) {
-        var xn = start + n * distance;
-        xCoordinates.push(xn);
-    }
-    console.log (xCoordinates);
-    return xCoordinates;
-    
 }
 
-    catch(err) {
-        console.log (err);
-    }
-}
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
@@ -87,26 +63,41 @@ var ctx = canvas.getContext("2d");
 var x_positions = calcXpositions(20, 340, 23); //der Variablen x_positions wird der return-wert zugewiesen 
 var y_positions = sinusShape(x_positions);
 
-var test_positions = calcXpositions(3, 8, 2);
-if (test_positions + "" !== "3,8") {    
-    console.log("Test failed. Expected [3, 8]");
-}
+function unitTests() {
+    function trivialCase() {
+        var test_positions = calcXpositions(3, 8, 2);
+        if (test_positions + "" !== "3,8") {    
+            console.log("Test failed. Expected [3, 8]");
+        }
+    }
 
-var test_positions = calcXpositions(3, 8, 4);
-if (test_positions + "" !== "3,4.666666666666667,6.333333333333334,8") {     
-    console.log("Test failed. Expected [3,4.666666666666667,6.333333333333334,8]");    
-}
-//var test_positions = calcXpositions(3, 8, 1);
-//if (test_positions + "" !== "3,8") {    
-//    console.log("Test failed. Expected [3, 8]");
+    function normalCase() {
+        var test_positions = calcXpositions(3, 8, 4);
+        if (test_positions + "" !== "3,4.666666666666667,6.333333333333334,8") {     
+            console.log("Test failed. Expected [3,4.666666666666667,6.333333333333334,8]");    
+        }
+        //var test_positions = calcXpositions(3, 8, 1);
+        //if (test_positions + "" !== "3,8") {    
+        //    console.log("Test failed. Expected [3, 8]");
+    }
 
-var test_positions = calcXpositions(3,8,1);
-if (calcXpositions[2]<2){
-    console.log("yap!");
+    function errorCase() {
+        // test if calcXpositions throws an error if count < 2
+        try {
+            calcXpositions(3,8,1);
+            // if we reach this line, it means calcXpositions did not
+            // throw an error. (we would be inside the catch block if it did)
+            console.log("Test failed: calcXpositions should throw an error if count < 2.");
+        } catch(err) {}
+    }
+    trivialCase();
+    normalCase();
+    errorCase();
 }
 
 // im array von x_positions werden die x positionen benannt
 // anhand dieser werte kann die funktion sinusShape die y_positions berechnen
 //test(test_positions);
+unitTests();
 drawRope(x_positions, y_positions,6);
 console.log('Hallo Welt');
