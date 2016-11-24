ca24197a08e12fd99b04331d3d4645396dcdf467
BOZ2323 <lillibaaske@gmx.de>
2016-11-24 11:50:53 +0100

    Code review.

--- rope.js
+++ rope.js
@@ -178,32 +178,30 @@ function unitTests() {
     normalCase();
     errorCase();
 }
+////////////////////////////END OF UNITTESTS///////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 unitTests();
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-var x_positions = calcXpositions(20, 340, 23); //der Variablen x_positions wird der return-wert zugewiesen 
+var x_positions = calcXpositions(20, 340, 10); //der Variablen x_positions wird der return-wert zugewiesen 
 
 
 function drawFrame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    var ty = 0;
-    ctx.globalAlpha = 0.0;
 
-    for (var i = 1; i <= 10; i++) {
-        ty = ty + 0.06;
-        ctx.globalAlpha = ctx.globalAlpha + 0.1;
+    for (var i = 0; i < 10; i++) {
+        var dt = 0.06 * i;
+        ctx.globalAlpha =  0.1 * i;
 
-        var y_positions = sinusShape(x_positions, Date.now()/1000 + ty); // Date.now gibt ms aus. Daher Umrechnung von ms in s 
+        var y_positions = sinusShape(x_positions, Date.now()/1000 + dt); // Date.now gibt ms aus. Daher Umrechnung von ms in s 
         drawRope(x_positions, y_positions,6, "#296AE3", "#296AE3");
 
     }
 }
 var timer = setInterval(drawFrame, 20);
-
-////////////////////////////END OF UNITTESTS///////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 console.log('Hallo Welt');
 
